c596c1b8c87c357ccc5f71f43488f7e05df3f8ec
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2018-08-20 23:12:38 +0000

    Update Serial number input.ino

--- Serial number input.ino
+++ Serial number input.ino
@@ -8,8 +8,9 @@ Serial.begin(9600);
 void loop(){
     while(Serial.available()){
     input_buffer = Serail.read();
-    value = atoi(input_buffer);
+    value = atoi(input_buffer);         //convert char to int
     Serial.print(" Input Data : ");
     Serial.println(value);
+    
   }
 }
