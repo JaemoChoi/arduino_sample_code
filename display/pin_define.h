42bbf6e678a34abf9a4d2ade12f2f65007acdc37
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-08-08 18:43:00 +0900

    Create pin_define.h

---
+++ display/pin_define.h
@@ -0,0 +1,38 @@
+#define NITTO_LEFT_PIN          1
+#define NITTO_LEFT_NUM          1
+
+#define FRONT_LEFT_PIN          1
+#define FRONT_LEFT_NUM          1
+
+#define NITTO_RIGHT_PIN         1
+#define NITTO_RIGHT_NUM         1
+
+#define FRONT_RIGHT_PIN         1
+#define FRONT_RIGHT_NUM         1
+
+#define CHILLER_PIN             1
+#define CHILLER_NUM             1
+
+#define TOP_LEVEL_FRONT_PIN     1
+#define TOP_LEVEL_FRONT_NUM     1
+
+#define TOP_LEVEL_BACK_PIN      1
+#define TOP_LEVEL_BACK_NUM      1
+
+#define MIDDLE_LEVEL_FRONT_PIN  1
+#define MIDDLE_LEVEL_FRONT_NUM  1
+
+#define MIDDLE_LEVEL_BACK_PIN   1
+#define MIDDLE_LEVEL_BACK_NUM   1
+
+#define BOT_LEVEL_FRONT_PIN     1
+#define BOT_LEVEL_FRONT_NUM     1
+
+#define BOT_LEVEL_BACK_PIN      1
+#define BOT_LEVEL_BACK_NUM      1
+
+#define SIDE_LEFT_PIN           1
+#define SIDE_LEFT_NUM           5
+
+#define SIDE_RIGHT_PIN          1
+#define SIDE_RIGHT_NUM          5
