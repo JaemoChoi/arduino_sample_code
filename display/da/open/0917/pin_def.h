8fee8426b2bb179902bc8fec21c94ae7c6f75a4e
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-09-17 01:26:52 +0900

    Create pin_def.h

---
+++ display/da/open/0917/pin_def.h
@@ -0,0 +1,23 @@
+#define SLL      4
+#define SLR      5
+#define BPL      6
+
+
+int SLL_STATUS = 0;
+int SLR_STATUS = 0;
+int BPL_STATUS = 0;
+int S11_STATUS = 0;
+int S12_STATUS = 0;
+int S13_STATUS = 0;
+int S14_STATUS = 0;
+
+int S21_STATUS = 0;
+int S22_STATUS = 0;
+int S23_STATUS = 0;
+int S24_STATUS = 0;
+
+int S31_STATUS = 0;
+int S32_STATUS = 0;
+int S33_STATUS = 0;
+int S34_STATUS = 0;
+
