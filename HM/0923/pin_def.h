20581dc76a6e52f42a0dc56cdd498d186824e926
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-09-24 03:52:03 +0900

    Create pin_def.h

---
+++ HM/0923/pin_def.h
@@ -0,0 +1,15 @@
+#define EAR_PIN         6
+#define SHOULDER_PIN    5
+#define HAND_PIN        4
+#define FOOT_PIN        3
+#define NECK_PIN        2
+
+#define EAR_NUM         30
+#define SHOULDER_NUM    30
+#define HAND_NUM        30
+#define FOOT_NUM        30
+#define NECK_NUM        30
+
+
+
+
