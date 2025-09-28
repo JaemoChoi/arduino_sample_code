8d7c3d2323c3ec0c839a7b669500dfac7009c718
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-09-28 17:46:44 +0900

    Add pin definitions and status variables

---
+++ HM/0928/humanG/pin_def.h
@@ -0,0 +1,14 @@
+#define EAR_PIN         6
+#define SHOULDER_PIN    5
+#define HAND_PIN        4
+#define FOOT_PIN        3
+
+#define EAR_NUM         9   // General
+#define SHOULDER_NUM    72
+#define HAND_NUM        30
+#define FOOT_NUM        30
+
+int EAR_STATUS = 0;
+int SHOULDER_STATUS = 0;
+int HAND_STATUS = 0;
+int FOOT_STATUS = 0;
