2a75f1b6033569a4b1c74d9e06834806fc509f70
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-10-01 14:27:19 +0900

    Create pin_def.h

---
+++ display/da/open/1001/pin_def.h
@@ -0,0 +1,58 @@
+#define SLF 4  // Side Light Left
+#define SLB 5  // Side Light Right
+#define SRF 6  // Chiller + Back Panel Light
+#define SRB 7  // Shelter Left Back
+//#define SLF       8     // Shelter Left Front
+#define SLL 9   // Shelter Right Back
+#define SLR 10  // Shelter Right Front
+#define F1 11
+#define F2 12
+#define F3 13
+
+#define CHI A4
+#define BPL A5
+
+#define LEFT_DOOR         80
+#define RIGHT_DOOR        81
+#define ALL_DOOR          82
+#define DRAWER_OPEN       83
+#define DRAWER_CLOSE      84
+#define LEFT_SLIDE_OPEN   85
+#define LEFT_SLIDE_CLOSE  86
+#define RIGHT_SLIDE_OPEN  87
+#define RIGHT_SLIDE_CLOSE 88
+#define CHILLER_UP        89
+#define CHILLER_DOWN      90
+#define DRAWER_MID_OPEN   95
+
+int SLL_STATUS = 0;
+int SLR_STATUS = 0;
+int BPL_STATUS = 0;
+int SLB_STATUS = 0;
+int SRB_STATUS = 0;
+int SLF_STATUS = 0;
+int SRF_STATUS = 0;
+
+int S11_STATUS = 0;
+int S12_STATUS = 0;
+int S13_STATUS = 0;
+int S14_STATUS = 0;
+
+int S21_STATUS = 0;
+int S22_STATUS = 0;
+int S23_STATUS = 0;
+int S24_STATUS = 0;
+
+int S31_STATUS = 0;
+int S32_STATUS = 0;
+int S33_STATUS = 0;
+int S34_STATUS = 0;
+
+int F1_STATUS = 0;
+int F2_STATUS = 0;
+int F3_STATUS = 0;
+
+int CHI_STATUS = 0;
+int SLIDING_L_STATUS = 0;
+int SLIDING_R_STATUS = 0;
+int CHILLER_MOTOR_STATUS = 0;
