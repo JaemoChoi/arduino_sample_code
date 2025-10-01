93241b577f7240eae97ea1f4159e6ae8a005efe9
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-10-01 14:23:18 +0900

    Create pin_def.h

---
+++ HM/1001/F/pin_def.h
@@ -0,0 +1,17 @@
+#define EAR_PIN        6
+#define SHOULDER_PIN    5
+#define HAND_PIN        4
+#define FOOT_PIN        3
+#define REAR_PIN        2
+
+#define EAR_NUM        60    // Friendly
+#define SHOULDER_NUM    72
+#define HAND_NUM        30
+#define FOOT_NUM        30
+#define NECK_NUM        72
+
+int EAR_STATUS = 0;
+int SHOULDER_STATUS = 0;
+int NECK_STATUS = 0;
+int HAND_STATUS = 0;
+int FOOT_STATUS = 0;
