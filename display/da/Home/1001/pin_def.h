78f30fe987deb07685c44887773a0e0df92b7a41
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-10-01 14:28:56 +0900

    Create pin_def.h

---
+++ display/da/Home/1001/pin_def.h
@@ -0,0 +1,24 @@
+#define TOP_FRONT         4
+#define TOP_MID           5
+#define DRAW_EDGE         6
+#define DRAW_BACK         7
+#define PDLC              A2
+
+#define DRAWER_OPEN     81
+#define DRAWER_CLOSE    80
+#define ICE             82
+
+
+int TOP_FRONT_STATUS = 0;
+int TOP_MID_STATUS = 0;
+int DRAW_EDGE_STATUS = 0;
+int DRAW_BACK_STATUS = 0;
+int TOP_BACK_STATUS = 0;
+int TOP_BACK1_STATUS = 0;
+int TOP_BACK2_STATUS = 0;
+int TOP_BACK3_STATUS = 0;
+int TOP_BACK4_STATUS = 0;
+int TOP_BACK5_STATUS = 0;
+int TOP_BACK6_STATUS = 0;
+
+int PDLC_STATUS = 0;
