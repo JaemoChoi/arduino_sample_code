58e5b95d0068c309ea1dea8ccae9808645742970
Jaemo Choi <38934903+JaemoChoi@users.noreply.github.com>
2025-10-01 14:31:02 +0900

    Create pin_def.h

---
+++ display/da/Stage/1001/pin_def.h
@@ -0,0 +1,65 @@
+#define BLR1       4
+#define BLR2       5
+#define BLR3       6
+#define BLR4       7
+#define BLRT       8
+
+#define BLL1       9
+#define BLL2      10
+#define BLL3      11
+#define BLL4      12
+#define BLLT      13
+
+// Down Light
+#define SLD1      25
+#define SLD2      26
+#define SLD3      27
+#define SRD1      22
+#define SRD2      23
+#define SRD3      24
+
+// Backside Light
+#define SLB1      30
+#define SLB2      31
+#define SLB3      32
+#define SLB4      33
+#define SRB1      34
+#define SRB2      35
+#define SRB3      36
+#define SRB4      37
+
+// PLDC
+#define PDLCL     41
+#define PDLCR     40
+
+int SLD1_STATUS = 0;
+int SLD2_STATUS = 0;
+int SLD3_STATUS = 0;
+int SRD1_STATUS = 0;
+int SRD2_STATUS = 0;
+int SRD3_STATUS = 0;
+
+int PDLCL_STATUS = 0;
+int PDLCR_STATUS = 0;
+
+int BLL1_STATUS = 0;
+int BLL2_STATUS = 0;
+int BLL3_STATUS = 0;
+int BLL4_STATUS = 0;
+int BLLT_STATUS = 0;
+
+int BLR1_STATUS = 0;
+int BLR2_STATUS = 0;
+int BLR3_STATUS = 0;
+int BLR4_STATUS = 0;
+int BLRT_STATUS = 0;
+
+int SLB1_STATUS = 0;
+int SLB2_STATUS = 0;
+int SLB3_STATUS = 0;
+int SLB4_STATUS = 0;
+
+int SRB1_STATUS = 0;
+int SRB2_STATUS = 0;
+int SRB3_STATUS = 0;
+int SRB4_STATUS = 0;
